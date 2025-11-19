5c4a758d533e15e1b51605e49e1c7e93e11058af
Dipu Biswas <12biswasdipu@gmail.com>
2025-11-19 19:07:15 +0530

    Environment Done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1 className='text-6xl text-center font-poppins font-black text-transparent bg-clip-text bg-linear-to-r from-red-400 to-green-500'>Hello World</h1>
+    </div>
+  )
+}
+
+export default page
